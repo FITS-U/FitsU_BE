7521bb563d8fe3ae20733ca6954a494c45bb5488
seyeon326 <lseyeon0723@gmail.com>
2024-11-12 22:28:19 +0900

    feat: product crud -> 권한해결필요

--- shop/src/main/java/com/example/shop/controller/ProductController.java
+++ shop/src/main/java/com/example/shop/controller/ProductController.java
@@ -35,4 +35,28 @@ public class ProductController {
         List<ProductResponse> mySales = productService.getMySales(userId);
         return ResponseEntity.ok(mySales);
     }
+
+    // 403 error : 클라이언트 권한 없음
+    @PostMapping
+    public ResponseEntity<ProductResponse> addProduct(
+            @RequestHeader UUID userId, @RequestBody ProductRequest request) {
+        ProductResponse productResponse = productService.addProduct(userId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    }
+
+    // 403 error
+    @PutMapping("/{productId}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @RequestHeader UUID userId, @PathVariable Long productId, @RequestBody ProductRequest request) {
+        ProductResponse productResponse = productService.updateProduct(userId, productId, request);
+        return ResponseEntity.ok(productResponse);
+    }
+
+    // 403 error
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<ProductResponse> deleteProduct(
+            @RequestHeader UUID userId, @PathVariable Long productId) {
+        productService.deleteProduct(userId, productId);
+        return ResponseEntity.ok(null);
+    }
 }
