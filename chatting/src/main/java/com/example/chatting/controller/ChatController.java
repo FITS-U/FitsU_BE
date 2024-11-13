85ea1362c59e942df394a690bdd82583413d2681
singersongha <songha714@naver.com>
2024-11-13 09:27:07 +0900

    chore: 엔드포인트 수정

--- chatting/src/main/java/com/example/chatting/controller/ChatController.java
+++ chatting/src/main/java/com/example/chatting/controller/ChatController.java
@@ -11,31 +11,32 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/v1/chats")
 public class ChatController {
 
     private final ChatService chatService;
 
-    @GetMapping("/chats/{userId}")
+    @GetMapping("/{userId}")
     public List<ChatResponse> getChatRoomList(@PathVariable UUID userId) {
         return chatService.getChatRoomList(userId);
     }
 
-    @GetMapping("/chats/{userId}/{chatRoomId}")
+    @GetMapping("/{userId}/{chatRoomId}")
     public List<ChatResponse> getRoomById(@PathVariable UUID userId, @PathVariable Long chatRoomId) {
         return chatService.getRoomById(chatRoomId, userId);
     }
 
-    @GetMapping("/chats/count/{productId}")
+    @GetMapping("/count/{productId}")
     public int getCountRooms(@PathVariable Long productId) {
         return chatService.getCountRooms(productId);
     }
 
-    @PostMapping("/chats")
+    @PostMapping("/")
     public ChatResponse addChatRoom(@RequestBody ChatRoom chatRoom) {
         return chatService.addChatRoom(chatRoom);
     }
 
-    @DeleteMapping("/chats/{chatRoomId}")
+    @DeleteMapping("/{chatRoomId}")
     public void deleteChatRoom(@PathVariable Long chatRoomId) {
         chatService.deleteChatRoom(chatRoomId);
     }
