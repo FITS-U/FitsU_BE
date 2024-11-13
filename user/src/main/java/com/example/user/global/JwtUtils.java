c868f336dfc5da1a54a67fd4ab3c86fce2a23e3e
seyeon326 <lseyeon0723@gmail.com>
2024-11-13 14:44:58 +0900

    feat: config

---
+++ user/src/main/java/com/example/user/global/JwtUtils.java
@@ -0,0 +1,42 @@
+package com.example.user.global;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private final long expiration;
+    private final SecretKey key;
+
+    public JwtUtils(
+            @Value("${jwt.expiration}") long expiration,
+            @Value("${jwt.secret}") String secret) {
+        this.expiration = expiration;
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String userName) {
+        Date expirationDate = new Date(System.currentTimeMillis() + expiration);
+        return Jwts.builder()
+                .subject(userName)
+                .expiration(expirationDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public String parseToken(String token) {
+        Claims payload = (Claims) Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parse(token)
+                .getPayload();
+        return payload.getSubject();
+    }
+}
