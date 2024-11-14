31fbe39dbebe4f93412a142221e72c663b2ecf2a
singersongha <songha714@naver.com>
2024-11-14 19:29:56 +0900

    chore: account-service 엔드포인트 수정

--- account/src/main/java/com/example/consumption/controller/AccountController.java
+++ account/src/main/java/com/example/consumption/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.example.consumption.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class AccountController {
 
     private final AccountService accountService;
