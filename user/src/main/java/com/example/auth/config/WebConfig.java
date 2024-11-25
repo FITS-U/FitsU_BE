aba076ebe04391b2592e4c91c4ec5f876333348b
singersongha <songha714@naver.com>
2024-11-25 18:40:40 +0900

    chore: cors 에러 처리

---
+++ user/src/main/java/com/example/auth/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // 자격 증명 허용
+    }
+}
