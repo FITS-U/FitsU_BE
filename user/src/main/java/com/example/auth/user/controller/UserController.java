d82ae6b5c88ac5903a1a90ecd7dbcddf7b5efcaf
singersongha <songha714@naver.com>
2024-11-25 21:34:40 +0900

    chore: cors 에러 수정

--- user/src/main/java/com/example/auth/user/controller/UserController.java
+++ user/src/main/java/com/example/auth/user/controller/UserController.java
@@ -3,17 +3,17 @@ package com.example.auth.user.controller;
 import com.example.auth.user.domain.dto.LoginRequest;
 import com.example.auth.user.domain.dto.RegisterRequest;
 import com.example.auth.user.domain.dto.VerificationRequest;
-import com.example.auth.user.service.SmsService;
 import com.example.auth.user.service.UserService;
 import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-@Transactional
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
