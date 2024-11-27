5bf2744971f27e863615e0da564b1d5e76275e4f
singersongha <songha714@naver.com>
2024-11-27 15:00:57 +0900

    chore: cors 에러 처리

--- card/src/main/java/com/example/card/controller/CardController.java
+++ card/src/main/java/com/example/card/controller/CardController.java
@@ -5,16 +5,14 @@ import com.example.card.response.CardResponse;
 import com.example.card.response.CategoryResponse;
 import com.example.card.service.CardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/cards")
+@CrossOrigin("http://localhost:3000")
 public class CardController {
 
     private final CardService cardService;
