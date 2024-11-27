43450fdec0615d0ab6c2a3ec6aa57d14db8ba8d1
singersongha <songha714@naver.com>
2024-11-27 20:09:35 +0900

    feat: 토큰 검증 api 구현

--- user/src/main/java/com/example/auth/user/controller/UserController.java
+++ user/src/main/java/com/example/auth/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.auth.user.controller;
 
+import com.example.auth.global.JwtUtils;
 import com.example.auth.user.domain.dto.LoginRequest;
 import com.example.auth.user.domain.dto.RegisterRequest;
 import com.example.auth.user.domain.dto.VerificationRequest;
 import com.example.auth.user.service.UserService;
 import lombok.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final JwtUtils jwtUtils;
 
    @PostMapping("/verify-code")
    public ResponseEntity<String> verifyCode(@RequestBody VerificationRequest request){
@@ -34,6 +37,16 @@ public class UserController {
     public ResponseEntity<String> register(@RequestBody RegisterRequest request, @RequestHeader("Authorization") String token) {
         String jwtToken = userService.register(request, token.substring(7));
         return ResponseEntity.ok(jwtToken);
+    }
+
+    @GetMapping("/validate-token")
+    public String validateToken(@RequestHeader("Authorization") String authorization) {
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            throw new AccessDeniedException("유효한 인증 토큰이 필요합니다.");
+        }
+
+        String token = authorization.substring(7);
+        return jwtUtils.parseToken(token); // 사용자 ID 반환
     }
 
 }
