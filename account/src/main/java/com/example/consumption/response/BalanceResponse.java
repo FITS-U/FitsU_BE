b80d76cd12a86be6d959ad26a7bbbbb9dccabcaf
singersongha <songha714@naver.com>
2024-12-05 11:36:15 +0900

    chore: 잔액 차감 api 로직 수정

--- account/src/main/java/com/example/consumption/response/BalanceResponse.java
+++ account/src/main/java/com/example/consumption/response/BalanceResponse.java
@@ -8,14 +8,12 @@ import java.util.UUID;
 
 @Builder
 public record BalanceResponse(
-        UUID userId,
         String bankName,
         String accountNum,
         Double balance
 ) {
     public static BalanceResponse from(UserAccount userAccount, Bank bank) {
         return new BalanceResponse(
-                userAccount.getUserId(),
                 bank.getBankName(),
                 userAccount.getAccountNum(),
                 userAccount.getBalance()
