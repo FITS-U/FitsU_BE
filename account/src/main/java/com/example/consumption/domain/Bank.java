f2f32e84d8e2aac23747ff3fb6382900466f04eb
singersongha <songha714@naver.com>
2024-12-10 15:29:24 +0900

    chore: imageUrl 컬럼 추가, 조회 api 수정

--- account/src/main/java/com/example/consumption/domain/Bank.java
+++ account/src/main/java/com/example/consumption/domain/Bank.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "BANK")
@@ -15,4 +17,6 @@ public class Bank {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bankId;
     private String bankName;
+    @Column(length = 1000)
+    private String imageUrl;
 }
