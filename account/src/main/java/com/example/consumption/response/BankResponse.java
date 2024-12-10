a6e52eb98a025ee85907c0260c855ccfc33adbd6
singersongha <songha714@naver.com>
2024-12-10 16:43:58 +0900

    chore: bank 엔티티 수정 및 조회 response 수정

--- account/src/main/java/com/example/consumption/response/BankResponse.java
+++ account/src/main/java/com/example/consumption/response/BankResponse.java
@@ -4,9 +4,10 @@ import com.example.consumption.domain.Bank;
 import lombok.Builder;
 
 @Builder
-public record BankResponse(Long bankId, String bankName) {
+public record BankResponse(Long bankId, String bankName, String imageUrl) {
 
     public static BankResponse from (Bank bank){
-        return new BankResponse(bank.getBankId(), bank.getBankName());
+
+        return new BankResponse(bank.getBankId(), bank.getBankName(), bank.getImageUrl());
     }
 }
